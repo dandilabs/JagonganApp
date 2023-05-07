62c3a154a0d4cd198b8227cea040ca9569963939
unknown <dandihermawan87@gmail.com>
2023-05-07 16:21:36 +0700

    StylingComponent

--- App.tsx
+++ App.tsx
@@ -1,118 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { Component } from 'react';
+import {View, Text, Image, ScrollView, TextInput, StyleSheet} from 'react-native';
+import laptop from './laptop.jpg';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <StylingReactNativeCompnent />
+  )
+};
+
+const StylingReactNativeCompnent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>INi STYLING COMPONENT</Text>
+      <View 
+      style={{
+        width:100, 
+        height:100, 
+        backgroundColor:'#3498db',
+        borderWidth: 2,
+        borderColor:'#e74c3c',
+        marginTop: 20,
+        marginLeft:20
+      }} />
+      <View style={{padding:12, backgroundColor:'#F2F2F2',width:212, borderRadius:8}}>
+        <Image source={laptop} style={{width:188, height:107, borderRadius:8}} />
+        <Text style={{fontSize:14, fontWeight:'bold', marginTop:16}}>New Macbook PRO 2019</Text>
+        <Text style={{fontSize:12,fontWeight:'bold', marginTop:12,color: '#F2994A'}}>
+            Rp.25.000.000
+        </Text>
+        <Text style={{fontSize:12,fontWeight:'bold',marginTop:12}}>Jakarta Barat </Text>
+        <View style={{
+          backgroundColor: '#2ecc71',
+          paddingVertical:6,
+          borderRadius:25,
+          marginTop:20
+          }}>
+          <Text 
+          style={{
+            fontSize:14, 
+            fontWeight:'bold', 
+            color:'white',
+            textAlign:'center'
+            }}>
+              Beli
+            </Text>
+        </View>
+      </View>
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  text:{
+    fontSize:18,
+    fontWeight:'bold',
+    color: '#1abc9c',
+    marginLeft:20,
+    marginTop:40
+  }
+})
+
+const SampleComponent = () => {
+  return (
+    <View>
+      <View style={{width: 80, heigh: 30, backgroundColor:'#2ecc71'}}/>
+      <Text>Dandi</Text>
+      <Text>Hermawan</Text>
+      <Photo/>
+      <TextInput style={{borderWidth:1}}/>
+      <BoxGreen/>
+      <Profile/>
+    </View>
+  )
+}
+
+const Photo = () => {
+  return <Image source={{uri:'https://placeimg.com/640/480/tech'}} style={{width:100, height:100}}/>
+};
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini Component dari class</Text>
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image 
+        source={{uri:'https://placeimg.com/640/480/animals'}} 
+        style ={{width:100, height:100, borderRadius:50}} />
+        <Text style={{color:'blue' , fontSize:24}}>Ini adalah hewan</Text>
+      </View>
+    )
+  }
+}
 
 export default App;
